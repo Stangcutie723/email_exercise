2b4285f2dad6ba39ab2265d6175b31ccff780b00
Stangcutie723 <Stangcutie723@gmail.com>
2013-08-07 13:57:29 -0700

    setting up email exercise number 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmailExercise::Application.config.secret_key_base = '2abf9f2352c66d246ba61ace75c34f3d40babdaaefc076ab7d497aee22444e5f0bba019fe054c50cfc3e80a621cf52c738993017544b2714181d4717cb642f67'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmailExercise::Application.config.session_store :cookie_store, key: '_email_exercise_session'
